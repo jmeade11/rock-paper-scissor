63ac6d713afd250015821ffb6e659ecd81e0347b
jmeade11 <hobokenjam@yahoo.com>
2022-12-05 12:04:58 -0500

    Add Modal component
    Adds a reusable component for creating a modal
    that can be passed a title and body content.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,71 @@
+import { useLayoutEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import styles from './Modal.module.css';
 
-export default function Modal() {
-	return <div className={styles.Modal}>Modal</div>;
+function createModalRootElement() {
+	const modalRoot = document.createElement('div');
+	modalRoot.setAttribute('id', 'modalRoot');
+	document.body.appendChild(modalRoot);
+
+	return modalRoot;
+}
+
+export default function Modal({
+	handleClose,
+	open = false,
+	title = '',
+	children,
+}) {
+	const modalRoot = useRef(
+		document.getElementById('modalRoot') || createModalRootElement()
+	);
+
+	useLayoutEffect(() => {
+		if (!open) return;
+		const escapeToClose = (event) => {
+			if (event.key !== 'Escape') return;
+			handleClose(false);
+		};
+
+		document.addEventListener('keyup', escapeToClose);
+
+		document.body.style.overflow = 'hidden';
+
+		return () => {
+			document.body.style = '';
+
+			document.removeEventListener('keyup', escapeToClose);
+		};
+	});
+
+	if (!open) return null;
+
+	const modalContent = (
+		<div
+			className={styles.backdrop}
+			onClick={() => handleClose(false)}
+			data-testid="backdrop"
+		>
+			<div
+				role="dialog"
+				id="dialog"
+				aria-modal="true"
+				className={styles.modal}
+				aria-labelledby="modalTitle"
+				onClick={(event) => event.stopPropagation()}
+			>
+				<header className={styles.modalHeader}>
+					<h2 id="modalTitle" className={styles.modalTitle}>
+						{title}
+					</h2>
+					<button onClick={() => handleClose(false)} className={styles.close}>
+						<span className="sr-only">Close Modal</span>
+					</button>
+				</header>
+				<div className={styles.modalBody}>{children}</div>
+			</div>
+		</div>
+	);
+
+	return ReactDOM.createPortal(modalContent, modalRoot.current);
 }
--- src/components/Modal/tests/Modal.test.js
+++ src/components/Modal/tests/Modal.test.js
@@ -1,8 +1,48 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Modal from '..';
 
-describe('Modal component tests', () => {
-	it('renders without crashing', () => {
-		render(<Modal />);
-	});
+test('modal shows the title, children and a close button', () => {
+	render(
+		<Modal title="modal title" open={true}>
+			<div>test</div>
+		</Modal>
+	);
+
+	expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
+		/modal title/i
+	);
+	expect(screen.getByText(/test/i)).toBeInTheDocument();
+	expect(
+		screen.getByRole('button', { name: /close modal/i })
+	).toBeInTheDocument();
+});
+
+test('modal close button handles close', () => {
+	const handleClose = jest.fn();
+
+	render(<Modal handleClose={handleClose} open={true}></Modal>);
+
+	userEvent.click(screen.getByText(/close modal/i));
+
+	expect(handleClose).toHaveBeenCalledTimes(1);
+});
+
+test('click on modal backdrop handles close', () => {
+	const handleClose = jest.fn();
+
+	render(<Modal handleClose={handleClose} open={true}></Modal>);
+
+	userEvent.click(screen.getByTestId('backdrop'));
+
+	expect(handleClose).toHaveBeenCalledTimes(1);
+});
+
+test('modal is closed when escape key is pressed', () => {
+	const handleClose = jest.fn();
+
+	render(<Modal handleClose={handleClose} open={true}></Modal>);
+
+	userEvent.keyboard('{Escape}');
+	expect(handleClose).toHaveBeenCalledTimes(1);
 });
